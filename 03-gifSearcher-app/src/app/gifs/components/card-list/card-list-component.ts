ae9784bf03cf28825f42445ce3bb26e9bc63ba50
Juansa García <dedalus1977@gmail.com>
2023-11-13 12:09:18 +0100

    loader and card component, app finished

--- 03-gifSearcher-app/src/app/gifs/components/card-list/card-list-component.ts
+++ 03-gifSearcher-app/src/app/gifs/components/card-list/card-list-component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { Gif } from '../../interfaces/gifs.interfaces';
 
 
+
+
 @Component({
   selector: 'gifs-card-list',
   templateUrl: 'card-list-component.html'
