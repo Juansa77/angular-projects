e5bc4da41c0404bc259845b3ab5aced04497a71c
Juansa García <dedalus1977@gmail.com>
2023-11-29 13:19:46 +0100

    added card and detail

--- 06-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class HeroesService {
@@ -14,5 +15,17 @@ export class HeroesService {
   getHeroes():Observable<Hero[]>{
     return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes`)
   }
+
+
+  getHeroById(id:string):Observable<Hero|undefined>{
+    return this.httpClient.get<Hero>(`${this.baseUrl}/heroes/${id}`).pipe(
+      //*of es un método de rxjs, define un observable que va a devolver, en este caso, undefined
+      catchError(error => of(undefined))
+    )
+  }
+
+getSuggestions(query:string):Observable<Hero[]>{
+  return this.httpClient.get<Hero[]>(`/heroes?q=${query}&_limit=6`)
+}
 
 }
