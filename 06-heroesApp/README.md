6546ea448627ec359809fe2be2daa76379eb2b26
Juansa García <dedalus1977@gmail.com>
2023-11-30 12:40:17 +0100

    added form and CRUD services

--- 06-heroesApp/README.md
+++ 06-heroesApp/README.md
@@ -5,4 +5,4 @@
 1. Clonar el proyecto
 2. Ejecutar npm install
 3. Levantar backend "npm run backend"
-4. Ejecutar npm start o ng s --o
+4. Ejecutar "npm start" o "ng s --o"
--- 06-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -3,29 +3,48 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class HeroesService {
+  private baseUrl: string = environments.baseUrl;
+  constructor(private httpClient: HttpClient) {}
 
-
-  private baseUrl:string = environments.baseUrl
-  constructor(private httpClient: HttpClient) { }
-
-  getHeroes():Observable<Hero[]>{
-    return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes`)
+  getHeroes(): Observable<Hero[]> {
+    return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes`);
   }
 
-
-  getHeroById(id:string):Observable<Hero|undefined>{
+  getHeroById(id: string): Observable<Hero | undefined> {
     return this.httpClient.get<Hero>(`${this.baseUrl}/heroes/${id}`).pipe(
       //*of es un método de rxjs, define un observable que va a devolver, en este caso, undefined
-      catchError(error => of(undefined))
-    )
+      catchError((error) => of(undefined))
+    );
   }
 
-getSuggestions(query:string):Observable<Hero[]>{
-  return this.httpClient.get<Hero[]>(`/heroes?q=${query}&_limit=6`)
-}
+  getSuggestions(query: string): Observable<Hero[]> {
+    return this.httpClient.get<Hero[]>(
+      `${this.baseUrl}/heroes?q=${query}&_limit=6`
+    );
+  }
 
+  addHero(hero: Hero): Observable<Hero> {
+    return this.httpClient.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) throw Error('Hero id is required');
+    return this.httpClient.patch<Hero>(
+      `${this.baseUrl}/heroes/${hero.id}`,
+      hero
+    );
+  }
+
+  //*Función que utiliza el observable para devolver un booleano de confirmación
+  deleteHeroById(id: string): Observable<boolean> {
+    if (!id) throw Error('Hero id is required');
+    return this.httpClient.delete(`${this.baseUrl}/heroes/${id}`).pipe(
+      catchError((error) => of(false)),
+      map((resp) => true)
+    );
+  }
 }
