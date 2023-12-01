3dd399668de13397c0e030f79aa88c9f35f3e60f
Juansa García <dedalus1977@gmail.com>
2023-12-01 12:37:56 +0100

    added form integration

--- 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,16 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit{
+  public heroForm = new FormGroup({
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('',{nonNullable:true}),
+    publisher: new FormControl<Publisher>(Publisher.MarvelComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
 
-  public publishers=[
-    {id:"DC Comics", desc:"DC-Comics"},
-    {id:"Marvel Comics", desc:"Marvel-comics"}
-  ]
+  constructor(private heroService: HeroesService, private activatedRoute: ActivatedRoute,
+    private router:Router){}
 
+
+  ngOnInit(): void {
+
+  }
+
+
+  get currentHero():Hero{
+    const hero= this.heroForm.value as Hero
+    return hero;
+  }
+  onSubmit():void{
+
+    if(this.heroForm.invalid)return;
+  if(this.currentHero.id){
+    this.heroService.updateHero(this.currentHero).subscribe(hero => {
+      //TODO:Show snackbar
+    } );
+
+    return
+  }
+
+  this.heroService.addHero(this.currentHero).subscribe(hero =>{
+    //TODO: show snackbar and navigate
+  })
+  }
+
+  public publishers = [
+    { id: 'DC Comics', desc: 'DC-Comics' },
+    { id: 'Marvel Comics', desc: 'Marvel-comics' },
+  ];
 }
