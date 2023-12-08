859cd8f356daa5d58a33251afcdef7044fb28552
Juansa García <dedalus1977@gmail.com>
2023-12-08 19:27:37 +0100

    added validations in dinamic form

--- 07-formApp/formApp/src/app/reactive/pages/dinamic-page/dynamic-page.component.ts
+++ 07-formApp/formApp/src/app/reactive/pages/dinamic-page/dynamic-page.component.ts
@@ -11,7 +11,7 @@ export class DynamicPageComponent {
 
   public myForm:FormGroup= this.fb.group({
     name: ["", [Validators.required,Validators.minLength(3)]],
-    favouriteGames: this.fb.array([
+    favoriteGames: this.fb.array([
       ["Metal Gear", Validators.required],
       ["Alan Wake", Validators.required],
       ["Death Stranding", Validators.required]
@@ -21,8 +21,38 @@ export class DynamicPageComponent {
 
   constructor(private fb: FormBuilder){}
 
-  get gameList(){
-    return this.myForm.get("favouriteGames") as FormArray
+  get favoriteGames(){
+    return this.myForm.get("favoriteGames") as FormArray
+  }
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  isValidFieldInArray(formArray:FormArray, index:number){
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    //si el formulario no tiene ese campo o no tiene errores, return null
+    if (!this.myForm.controls[field]) return null;
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const iterator of Object.keys(errors)) {
+      switch (iterator) {
+        case 'required':
+          return 'This field is required';
+
+        case 'minlength':
+          return `This field requires at least ${errors['minlength'].requiredLength} characters`;
+      }
+    }
+
+    return null;
   }
 
   onSubmit():void{
