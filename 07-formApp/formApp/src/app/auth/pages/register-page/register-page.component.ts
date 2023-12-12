f83eb738ae5545ad45131b29b6711f257e2fed3f
Juansa García <dedalus1977@gmail.com>
2023-12-12 12:46:51 +0100

    added validators services observable

--- 07-formApp/formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formApp/formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { cantBeStrider } from 'src/app/shared/validators/validators.functions';
+import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from 'src/app/shared/services/validators.service';
+import { EmailValidatorService } from 'src/app/shared/validators/email-validators.service';
+// import * as customValidators from 'src/app/shared/validators/validators.functions';
 
 @Component({
   selector: 'app-register-page',
@@ -11,18 +13,27 @@ import { cantBeStrider } from 'src/app/shared/validators/validators.functions';
 export class RegisterPageComponent {
 
   public myForm: FormGroup= this.fb.group({
-    name:["", [Validators.required ]],
-    email:["", [Validators.required]],
-    userName: ["", [Validators.required, cantBeStrider]],
+    name:["", [Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern) ]],
+    email:["", [Validators.required, Validators.pattern(this.validatorsService.emailPattern)], [this.emailValidator]],
+    userName: ["", [Validators.required, this.validatorsService.cantBeStrider]],
     password: ["", [Validators.required, Validators.minLength(6)]],
     password2: ["", [Validators.required]],
+  },
+  //* objeto para validadores grupales en todos los campois
+  {
+    //* validador grupal
+    validators:[
+      this.validatorsService.isFieldOneEqualsFieldTwo("password","password2"),
+    ]
   })
 
-  constructor(private fb: FormBuilder ){}
+  constructor(private fb: FormBuilder,
+    public validatorsService: ValidatorsService,
+    private emailValidator: EmailValidatorService ){}
 
 
   isValidField(field:string){
-    //TODO: Obtener validacion
+   return this.validatorsService.isValidField(this.myForm, field)
   }
   onSubmit(){
     this.myForm.markAllAsTouched()
