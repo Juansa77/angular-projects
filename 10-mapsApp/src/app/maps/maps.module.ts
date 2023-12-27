73265337768830926fd9f5e7368929b53f2dc77d
Juansa García <dedalus1977@gmail.com>
2023-12-27 11:11:23 +0100

    standAlone components

--- 10-mapsApp/src/app/maps/maps.module.ts
+++ 10-mapsApp/src/app/maps/maps.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import * as mapboxgl from 'mapbox-gl';
 
-(mapboxgl as any).accessToken = environment.access_token
+(mapboxgl as any).accessToken = environment.access_token;
 
 import { MapsRoutingModule } from './maps-routing.module';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
+
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
@@ -14,18 +14,23 @@ import { MarkersPageComponent } from './pages/markers-page/markers-page.componen
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
 import { environment } from 'src/environments/environment';
+import { CounterAloneComponent } from '../alone/components/counter-alone/counter-alone.component';
+import { SideMenuComponent } from '../alone/components/side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
-    SideMenuComponent,
     MiniMapComponent,
-    SideMenuComponent,
     MapsLayoutComponent,
     FullScreenPageComponent,
     MarkersPageComponent,
     PropertiesPageComponent,
     ZoomRangePageComponent,
   ],
-  imports: [CommonModule, MapsRoutingModule],
+  imports: [
+    CommonModule,
+    MapsRoutingModule,
+    CounterAloneComponent,
+    SideMenuComponent,
+  ],
 })
 export class MapsModule {}
--- 10-mapsApp/src/app/maps/pages/properties-page/properties-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CounterAloneComponent } from '../../../alone/components/counter-alone/counter-alone.component';
 
 interface House {
   title: string;
@@ -7,6 +8,7 @@ interface House {
 }
 
 @Component({
+
   selector: 'app-properties-page',
   templateUrl: './properties-page.component.html',
   styleUrls: ['./properties-page.component.css']
