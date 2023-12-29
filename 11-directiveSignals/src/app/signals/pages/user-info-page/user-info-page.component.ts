8a830a90f0f647cd2012eb8b524a2369ad63b2a1
Juansa García <dedalus1977@gmail.com>
2023-12-29 13:04:22 +0100

    signals

--- 11-directiveSignals/src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ 11-directiveSignals/src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { UsersServiceService } from '../../services/users-service.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-user-info-page',
   templateUrl: './user-info-page.component.html',
-  styleUrls: ['./user-info-page.component.css']
+  styleUrls: ['./user-info-page.component.css'],
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
+  ngOnInit(): void {
+    this.loadUser(this.userID());
+  }
 
+  loadUser(id: number) {
+    if (id <= 0) return;
+    this.userID.set(id);
+    this.currentUser.set(undefined)
+    this.userService.getUserById(id).subscribe(user =>{
+    this.currentUser.set(user)
+    })
+  }
+
+  private userService = inject(UsersServiceService);
+  public userID = signal(1);
+
+  public currentUser = signal<User | undefined>(undefined);
+  public userWasFound = signal(true);
 }
